b35c5f5667d3360f546488ed6b29b65b873df359
Omar Almalol <omar.alma3lol@gmail.com>
2021-12-26 20:12:30 +0200

    initial commit

---
+++ .zshrc
@@ -0,0 +1,24 @@
+export ZSH_CUSTOM="$HOME/.dotfiles/zsh"
+export WORK="$HOME/work"
+
+ZSH_THEME="rkj-repos"
+
+plugins=(
+	git
+	npm
+	history-substring-search
+	docker
+	zsh-syntax-highlighting
+	zsh-syntax-highlighting
+)
+
+source $ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+fpath=($ZSH_CUSTOM/functions $ZSH_CUSTOM/completions $fpath)
+autoload -U $ZSH_CUSTOM/functions/*(:t)
+autoload -U $ZSH_CUSTOM/completions/*(:t)
+
+alias lg="lazygit"
+alias g="git"
+alias ll="ls -lA"
+alias t="tree -I .git -a"
---
+++ install.sh
@@ -0,0 +1,65 @@
+# Variables
+ZSH="$HOME/.oh-my-zsh"
+ZSH_CUSTOM="$HOME/.dotfiles/zsh"
+DOTFILES=`pwd`
+package_manager_command=''
+
+# Checks
+case `cat /etc/os-release | grep ID_LIKE | sed -e 's/ID_LIKE=//g'` in
+	debian)
+		package_manager_command='apt install -y'
+		;;
+	arch)
+		package_manager_command='pacman -S --noconfirm'
+		;;
+esac
+
+# Functions
+mkdir_if_not_exists() {
+	if [ ! -d $1 ]; then
+		mkdir -p $1;
+	fi
+}
+clone_plugin_if_not_exists() {
+	if [ ! -d $2 ]; then
+		git clone $1 $2
+	fi
+}
+install_package_if_not_installed() {
+	result=$(which $1 2>&1)
+	if [[ "$result" == *"no $1"* ]]; then
+		`sudo $package_manager_command $1`;
+	fi
+}
+link_config_if_not_linked() {
+	if [ ! -f $2 ]; then
+		ln -s $1 $2
+	fi
+}
+
+# Required packages
+install_package_if_not_installed zsh
+install_package_if_not_installed git
+install_package_if_not_installed curl
+
+# Dirs
+mkdir_if_not_exists $ZSH
+mkdir_if_not_exists $ZSH_CUSTOM
+mkdir_if_not_exists $ZSH_CUSTOM/plugins
+
+# Config files
+link_config_if_not_linked $DOTFILES/.zshrc $HOME/.zshrc
+link_config_if_not_linked $DOTFILES/.gitconfig $HOME/.gitconfig
+
+# ZSH Plugins
+clone_plugin_if_not_exists https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+clone_plugin_if_not_exists https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
+clone_plugin_if_not_exists https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Oh-my-zsh
+result=$(sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 2>&1)
+if [[ "$result" != *"folder already exists"* ]]; then
+	echo "Done installing zsh";
+else
+	echo "Oh-my-zsh already exists."
+fi
