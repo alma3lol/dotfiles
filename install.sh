f612737c3881aef32e5e74bf934783ef326bc61f
Omar Almalol <omar.alma3lol@gmail.com>
2021-12-27 20:24:46 +0200

    fix: fixes stuck installer & corrects .zshrc config

--- install.sh
+++ install.sh
@@ -46,19 +46,15 @@ install_package_if_not_installed curl
 mkdir_if_not_exists $ZSH_CUSTOM
 mkdir_if_not_exists $ZSH_CUSTOM/plugins
 
-# Config files
-link_config_if_not_linked $DOTFILES/.zshrc $HOME/.zshrc
-link_config_if_not_linked $DOTFILES/.gitconfig $HOME/.gitconfig
-
 # ZSH Plugins
 clone_plugin_if_not_exists https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 clone_plugin_if_not_exists https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
 clone_plugin_if_not_exists https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # Oh-my-zsh
-result=$(sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 2>&1)
-if [[ "$result" == *"folder already exists"* ]]; then
-	echo "Oh-my-zsh already exists."
-else
-	echo "Done installing zsh";
-fi
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm $HOME/.zshrc
+
+# Config files
+link_config_if_not_linked $DOTFILES/.zshrc $HOME/.zshrc
+link_config_if_not_linked $DOTFILES/.gitconfig $HOME/.gitconfig
