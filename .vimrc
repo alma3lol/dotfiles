338fde4ef64a515679df0839c4727f13809e229f
Omar Almalol <omar.alma3lol@gmail.com>
2021-12-27 21:03:56 +0200

    feat: adds support for neovim & vim

---
+++ .vimrc
@@ -0,0 +1,52 @@
+syntax enable
+set termguicolors
+set number
+set relativenumber
+set autoread
+au FocusGained,BufEnter * :silent! !
+set encoding=utf-8
+set visualbell
+set backspace=indent,eol,start
+set nobackup
+set noswapfile
+set scrolloff=8
+set noexpandtab tabstop=4 shiftwidth=4 softtabstop=4 
+set autoindent
+set smartindent
+set showmode showcmd
+set ttyfast lazyredraw
+set showmatch
+set hlsearch incsearch ignorecase smartcase
+set hidden
+set wildmenu wildmode=list:longest,full
+set laststatus=2 statusline=%F
+set foldmethod=indent
+set foldnestmax=1
+set foldlevelstart=1
+set colorcolumn=80
+
+let g:netrw_browse_split = 3
+
+call plug#begin('~/.local/share/vim/plugged')
+Plug 'itchyny/lightline.vim'
+Plug 'tpope/vim-commentary'
+Plug 'tpope/vim-surround'
+Plug 'tpope/vim-vinegar'
+Plug 'APZelos/blamer.nvim'
+Plug 'fenetikm/falcon'
+Plug 'macguirerintoul/night_owl_light.vim'
+call plug#end()
+
+colorscheme night_owl_light
+
+let g:lightline = {
+	      \ 'colorscheme': 'PaperColor_light',
+      \ 'active': {
+	      \   'left': [ [ 'mode', 'paste' ],
+      \             [ 'readonly', 'absolutepath', 'modified' ] ]
+      \ }
+      \ }
+
+let g:blamer_enabled = 1
+let g:blamer_date_format = '%e %b %Y'
+highlight Blamer guifg=darkorange
--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ mkdir_if_not_exists() {
 		mkdir -p $1;
 	fi
 }
-clone_plugin_if_not_exists() {
+clone_if_not_exists() {
 	if [ ! -d $2 ]; then
 		git clone $1 $2
 	fi
@@ -31,7 +31,7 @@ install_package_if_not_installed() {
 		`sudo $package_manager_command $1`;
 	fi
 }
-link_config_if_not_linked() {
+link_if_not_linked() {
 	if [ ! -f $2 ]; then
 		ln -s $1 $2
 	fi
@@ -46,15 +46,35 @@ install_package_if_not_installed curl
 mkdir_if_not_exists $ZSH_CUSTOM
 mkdir_if_not_exists $ZSH_CUSTOM/plugins
 
+# (Neo)Vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+clone_if_not_exists https://github.com/alma3lol/neovim-config $DOTFILES/nvim-config
+
 # ZSH Plugins
-clone_plugin_if_not_exists https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-clone_plugin_if_not_exists https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
-clone_plugin_if_not_exists https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+clone_if_not_exists https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+clone_if_not_exists https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
+clone_if_not_exists https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm $HOME/.zshrc
 
 # Config files
-link_config_if_not_linked $DOTFILES/.zshrc $HOME/.zshrc
-link_config_if_not_linked $DOTFILES/.gitconfig $HOME/.gitconfig
+link_if_not_linked $DOTFILES/.zshrc $HOME/.zshrc
+link_if_not_linked $DOTFILES/.gitconfig $HOME/.gitconfig
+link_if_not_linked $DOTFILES/.vimrc $HOME/.vimrc
+link_if_not_linked $DOTFILES/nvim-init.vim $HOME/.config/nvim/init.vim
+link_if_not_linked $DOTFILES/nvim-config $HOME/.config/nvim/config
+
+# Initiating vim
+result=$(which vim 2>&1)
+if [[ "$result" == *"no vim"* ]]; then
+	vim +PlugInstall +qall
+fi
+
+# Initiating neovim
+result=$(which vim 2>&1)
+if [[ "$result" == *"no vim"* ]]; then
+	nvim --headless +PlugInstall +qa
+fi
