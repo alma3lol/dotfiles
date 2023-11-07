d885707d464660c14f5b57b4408e27c0d3fcc633
Omar Almalol <omar.alma3lol@gmail.com>
2023-11-07 15:24:43 +0200

    feat(neovim): adds snippets to typescript language

--- .config/nvim/lua/alma3lol/plugins/luasnip/snips/typescript.lua
+++ .config/nvim/lua/alma3lol/plugins/luasnip/snips/typescript.lua
@@ -1,51 +1,51 @@
 local function prequire(...)
-    local status, lib = pcall(require, ...)
-    if (status) then return lib end
-    return nil
+  local status, lib = pcall(require, ...)
+  if (status) then return lib end
+  return nil
 end
 
 local luasnip = prequire('luasnip')
 
-local fmt = require"luasnip.extras.fmt".fmt
+local fmt = require "luasnip.extras.fmt".fmt
 local s = luasnip.snippet
 local i = luasnip.insert_node
 local t = luasnip.text_node
 local f = luasnip.function_node
 local c = luasnip.choice_node
-local r = require"luasnip.extras".rep
-local u = function (index)
-    return f(function(args)
-        return args[index][1]:upper()
-    end, { index })
+local r = require "luasnip.extras".rep
+local u = function(index)
+  return f(function(args)
+    return args[index][1]:upper()
+  end, { index })
 end
-local l = function (index)
-    return f(function(args)
-        return args[index][1]:lower()
-    end, { index })
-end
-
-
-local lowercaseFirstLetter = function (index)
-    return f(function(args)
-        return args[index][1]:sub(1,1):lower()..args[index][1]:sub(2)
-    end, { index })
+local l = function(index)
+  return f(function(args)
+    return args[index][1]:lower()
+  end, { index })
 end
 
--- local uppercaseFirstLetter = function (index)
---     return f(function(args)
---         return args[index][1]:sub(1,1):upper()..args[index][1]:sub(2)
---     end, { index })
--- end
+
+local lowercaseFirstLetter = function(index)
+  return f(function(args)
+    return args[index][1]:sub(1, 1):lower() .. args[index][1]:sub(2)
+  end, { index })
+end
+
+local uppercaseFirstLetter = function(index)
+  return f(function(args)
+    return args[index][1]:sub(1, 1):upper() .. args[index][1]:sub(2)
+  end, { index })
+end
 
 return {
-    s('type', {
-        t('export type '), i(1, ''), t({' = {' , '    '}), i(0, ''), t({'', '}'}),
-    }),
-    s('tp', {
-        i(1, ''), t(': '), c(2, {t 'string', t 'number', t 'boolean'}), t(';'), i(0)
-    }),
-    s('athunk',
-        fmt([[
+  s('type', {
+    t('export type '), i(1, ''), t({ ' = {', '    ' }), i(0, ''), t({ '', '}' }),
+  }),
+  s('tp', {
+    i(1, ''), t(': '), c(2, { t 'string', t 'number', t 'boolean' }), t(';'), i(0)
+  }),
+  s('athunk',
+    fmt([[
                 export const {}Thunk = createAsyncThunk(
                   '{}/{}',
                   async ({}) => {{
@@ -53,15 +53,15 @@ return {
                   }}
                 );
             ]], {
-                i(1),
-                i(2),
-                r(1),
-                i(3),
-                i(4),
-            })
-    ),
-    s('rtqapi',
-        fmt([[
+      i(1),
+      i(2),
+      r(1),
+      i(3),
+      i(4),
+    })
+  ),
+  s('rtqapi',
+    fmt([[
 import {{ createApi }} from '@reduxjs/toolkit/query/react';
 import {{ {modelName}sModel }} from "../models";
 import {{ baseQuery, Count, Filter, FilterExcludingWhere, parseUri, Where }} from "./base.service";
@@ -130,60 +130,60 @@ export const {{
   use{modelName34}sBulkUpdateMutation,
 }} = {modelName35}sAPI;
         ]], {
-                modelName = i(1), -- IMPORTS
-                modelName1 = lowercaseFirstLetter(1), -- CREATE API
-                modelName2 = lowercaseFirstLetter(1), -- REDUCER PATH
-                modelName3 = lowercaseFirstLetter(1), -- CREATE MUTATION
-                modelName4 = r(1),
-                modelName5 = r(1),
-                idType = c(2, { t 'number', t 'string' }),
-                modelName6 = l(1),
-                modelName7 = lowercaseFirstLetter(1), -- FETCH QUERY
-                modelName8 = r(1),
-                idType1 = r(2),
-                modelName9 = r(1),
-                idType2 = r(2),
-                modelName10 = l(1),
-                modelName11 = lowercaseFirstLetter(1), -- FETCH ALL QUERY
-                modelName12 = r(1),
-                modelName13 = r(1),
-                idType3 = r(2),
-                modelName14 = l(1),
-                modelName15 = lowercaseFirstLetter(1), -- DELETE MUTATION
-                idType4 = r(2),
-                idType5 = r(2),
-                modelName16 = l(1),
-                modelName17 = lowercaseFirstLetter(1), -- BULK DELETE MUTATION
-                modelName18 = r(1),
-                idType6 = r(2),
-                modelName19 = l(1),
-                modelName20 = lowercaseFirstLetter(1), -- UPDATE MUTATION
-                modelName21 = r(1),
-                idType7 = r(2),
-                modelName22 = r(1),
-                idType8 = r(2),
-                modelName23 = l(1),
-                modelName24 = lowercaseFirstLetter(1), -- BULK UPDATE MUTATION
-                modelName25 = r(1),
-                modelName26 = r(1),
-                idType9 = r(2),
-                modelName27 = l(1),
-                modelName28 = r(1), -- EXPORTS
-                modelName29 = r(1),
-                modelName30 = r(1),
-                modelName31 = r(1),
-                modelName32 = r(1),
-                modelName33 = r(1),
-                modelName34 = r(1),
-                modelName35 = lowercaseFirstLetter(1),
-                modelName36 = lowercaseFirstLetter(1),
-                modelName37 = lowercaseFirstLetter(1),
-                modelName38 = lowercaseFirstLetter(1),
-                modelName39 = lowercaseFirstLetter(1),
-                modelName40 = lowercaseFirstLetter(1),
-                modelName41 = lowercaseFirstLetter(1),
-                modelName42 = lowercaseFirstLetter(1),
-                modelName43 = lowercaseFirstLetter(1),
-            })
-    ),
+      modelName = i(1),                     -- IMPORTS
+      modelName1 = lowercaseFirstLetter(1), -- CREATE API
+      modelName2 = lowercaseFirstLetter(1), -- REDUCER PATH
+      modelName3 = lowercaseFirstLetter(1), -- CREATE MUTATION
+      modelName4 = r(1),
+      modelName5 = r(1),
+      idType = c(2, { t 'number', t 'string' }),
+      modelName6 = l(1),
+      modelName7 = lowercaseFirstLetter(1), -- FETCH QUERY
+      modelName8 = r(1),
+      idType1 = r(2),
+      modelName9 = r(1),
+      idType2 = r(2),
+      modelName10 = l(1),
+      modelName11 = lowercaseFirstLetter(1), -- FETCH ALL QUERY
+      modelName12 = r(1),
+      modelName13 = r(1),
+      idType3 = r(2),
+      modelName14 = l(1),
+      modelName15 = lowercaseFirstLetter(1), -- DELETE MUTATION
+      idType4 = r(2),
+      idType5 = r(2),
+      modelName16 = l(1),
+      modelName17 = lowercaseFirstLetter(1), -- BULK DELETE MUTATION
+      modelName18 = r(1),
+      idType6 = r(2),
+      modelName19 = l(1),
+      modelName20 = lowercaseFirstLetter(1), -- UPDATE MUTATION
+      modelName21 = r(1),
+      idType7 = r(2),
+      modelName22 = r(1),
+      idType8 = r(2),
+      modelName23 = l(1),
+      modelName24 = lowercaseFirstLetter(1), -- BULK UPDATE MUTATION
+      modelName25 = r(1),
+      modelName26 = r(1),
+      idType9 = r(2),
+      modelName27 = l(1),
+      modelName28 = r(1), -- EXPORTS
+      modelName29 = r(1),
+      modelName30 = r(1),
+      modelName31 = r(1),
+      modelName32 = r(1),
+      modelName33 = r(1),
+      modelName34 = r(1),
+      modelName35 = lowercaseFirstLetter(1),
+      modelName36 = lowercaseFirstLetter(1),
+      modelName37 = lowercaseFirstLetter(1),
+      modelName38 = lowercaseFirstLetter(1),
+      modelName39 = lowercaseFirstLetter(1),
+      modelName40 = lowercaseFirstLetter(1),
+      modelName41 = lowercaseFirstLetter(1),
+      modelName42 = lowercaseFirstLetter(1),
+      modelName43 = lowercaseFirstLetter(1),
+    })
+  ),
 }
--- .config/nvim/lua/alma3lol/plugins/luasnip/snips/typescript/generated.lua
+++ .config/nvim/lua/alma3lol/plugins/luasnip/snips/typescript/generated.lua
@@ -1,8 +1,113 @@
-local ls = require("luasnip")
+local luasnip = require("luasnip")
 local fmt = require "luasnip.extras.fmt".fmt
-local s = ls.snippet
-local i = ls.insert_node
+local s = luasnip.snippet
+local i = luasnip.insert_node
+local t = luasnip.text_node
+local f = luasnip.function_node
+local c = luasnip.choice_node
+local r = require "luasnip.extras".rep
+local u = function(index)
+  return f(function(args)
+    return args[1][1]:upper()
+  end, { index })
+end
+local l = function(index)
+  return f(function(args)
+    return args[1][1]:lower()
+  end, { index })
+end
+
+
+local lowercaseFirstLetter = function(index)
+  return f(function(args)
+    return args[1][1]:sub(1, 1):lower() .. args[1][1]:sub(2)
+  end, { index })
+end
+
+local uppercaseFirstLetter = function(index)
+  return f(function(args)
+    return args[1][1]:sub(1, 1):upper() .. args[1][1]:sub(2)
+  end, { index })
+end
 
 return {
-	------------------------------------------------------ Snippets goes here
+  s(
+    "gqlresolver",
+    fmt([[
+import {{ Args, Mutation, Query, Resolver }} from '@nestjs/graphql';
+import {{ AppService }} from 'src/app.service';
+import {{ New{cName0} }} from 'src/graphql.schema';
+
+@Resolver('{cName1}')
+export class {cNames0}Resolver {{
+  constructor(private readonly service: AppService) {{}}
+
+  @Query('{sNames0}')
+  {sNames1}() {{
+    return this.service.{sName0}.findMany({{
+      include: {{ {include0} }},
+    }});
+  }}
+
+  @Query('{sName1}')
+  {sName2}(@Args('id') id: number) {{
+    return this.service.{sName3}.findUniqueOrThrow({{
+      where: {{ id }},
+      include: {{ {include1} }},
+    }});
+  }}
+
+  @Query('search{cNames1}')
+  search{cNames2}(@Args('name') name: string) {{
+    return this.service.{sName4}.findMany({{
+      where: {{
+        OR: [
+          {{ name: {{ contains: name.toLowerCase() }} }},
+          {{ nameAr: {{ contains: name }} }},
+        ],
+      }},
+      include: {{ {include2} }},
+    }});
+  }}
+
+  @Mutation('add{cName2}')
+  async add{cName3}(@Args('{sName5}') {sName6}: New{cName4}) {{
+    return this.service.{sName7}.create({{
+      data: {{
+        name: {sName8}.name,
+        nameAr: {sName9}.nameAr,
+        {}
+      }}
+    }});
+  }}
+}}
+]], {
+      cName0 = i(1),
+      cName1 = r(1),
+      cName2 = r(1),
+      cName3 = r(1),
+      cName4 = r(1),
+      cNames0 = i(2),
+      cNames1 = r(2),
+      cNames2 = r(2),
+        sName0 = lowercaseFirstLetter(1),
+        sName1 = lowercaseFirstLetter(1),
+        sName2 = lowercaseFirstLetter(1),
+        sName3 = lowercaseFirstLetter(1),
+        sName4 = lowercaseFirstLetter(1),
+        sName5 = lowercaseFirstLetter(1),
+        sName6 = lowercaseFirstLetter(1),
+        sName7 = lowercaseFirstLetter(1),
+        sName8 = lowercaseFirstLetter(1),
+        sName9 = lowercaseFirstLetter(1),
+        sNames0 = lowercaseFirstLetter(2),
+        sNames1 = lowercaseFirstLetter(2),
+      include0 = i(3),
+      include1 = r(3),
+      include2 = r(3),
+      i(4),
+    })
+  ),
+
+  ------------------------------------------------------ Snippets goes here
 }
