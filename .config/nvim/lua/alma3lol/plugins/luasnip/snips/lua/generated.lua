dbed7eff42ce2a188fe5234decab5ac074394b44
Omar Almalol <omar.alma3lol@gmail.com>
2023-11-07 15:24:33 +0200

    feat(neovim): adds snippets to lua language

--- .config/nvim/lua/alma3lol/plugins/luasnip/snips/lua/generated.lua
+++ .config/nvim/lua/alma3lol/plugins/luasnip/snips/lua/generated.lua
@@ -4,5 +4,21 @@ local s = ls.snippet
 local i = ls.insert_node
 
 return {
+s(
+	"newsnippetplaceholder",
+	fmt([=[
+local ls = require("luasnip")
+local fmt = require "luasnip.extras.fmt".fmt
+local s = ls.snippet
+local i = ls.insert_node
+
+return {{
+    ------------------------------------------------------ Snippets goes here
+}}
+]=], {
+		
+	})
+),
+
 	------------------------------------------------------ Snippets goes here
 }
