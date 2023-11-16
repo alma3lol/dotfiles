c1a5faac6eaf5f3100bb78ec3c7bf6d71fc14eed
Khaled Bawsh <khaled.bawsh@gmail.com>
2023-11-16 17:23:18 +0200

    feat(neovim): adds chat GPT edit with instructions bindings

--- .config/nvim/lua/alma3lol/plugins/chatgpt/bindings.lua
+++ .config/nvim/lua/alma3lol/plugins/chatgpt/bindings.lua
@@ -1,2 +1,4 @@
 vim.g.alma3lol.bindings.nnoremap('<leader>gpto', '<cmd>ChatGPT<CR>')
 vim.g.alma3lol.bindings.nnoremap('<leader>gpta', '<cmd>ChatGPTActAs<CR>')
+vim.g.alma3lol.bindings.nnoremap('<leader>gpte', '<cmd>ChatGPTEditWithInstructions<CR>')
+vim.g.alma3lol.bindings.xnoremap('<leader>gpte', '<cmd>ChatGPTEditWithInstructions<CR>')
--- .config/nvim/lua/alma3lol/plugins/chatgpt/init.lua
+++ .config/nvim/lua/alma3lol/plugins/chatgpt/init.lua
@@ -1,11 +1,15 @@
 local M = {
 	setup = function()
 		require('alma3lol.plugins.chatgpt.setup')
+	end,
+	bindings = function()
+		require('alma3lol.plugins.chatgpt.bindings')
 	end
 }
 
 M.init = function()
 	M.setup()
+	M.bindings()
 end
 
 return M
