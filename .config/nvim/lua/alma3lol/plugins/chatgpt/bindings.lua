88efe36a4fd90f373fd57ee5979a6f5dd388f87a
Khaled Bawsh <khaled.bawsh@gmail.com>
2023-11-16 15:38:59 +0200

    refactor(neovim): changes binding for chat GPT

--- .config/nvim/lua/alma3lol/plugins/chatgpt/bindings.lua
+++ .config/nvim/lua/alma3lol/plugins/chatgpt/bindings.lua
@@ -1,2 +1,2 @@
 vim.g.alma3lol.bindings.nnoremap('<leader>gpto', '<cmd>ChatGPT<CR>')
-vim.g.alma3lol.bindings.nnoremap('<leader>gptr', '<cmd>ChatGPTActAs<CR>')
+vim.g.alma3lol.bindings.nnoremap('<leader>gpta', '<cmd>ChatGPTActAs<CR>')
