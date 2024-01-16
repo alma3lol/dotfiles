356d8bc9421098f9bfa7d61b54d3cc5df285947c
Omar Almalol <omar.alma3lol@gmail.com>
2024-01-16 15:19:46 +0200

    feat(neovim): adds project key check for linear.app plugin

--- .config/nvim/lua/alma3lol/plugins/linear/setup.lua
+++ .config/nvim/lua/alma3lol/plugins/linear/setup.lua
@@ -1,4 +1,13 @@
 local home = vim.fn.expand("$HOME")
+
+local file_exists = function(name)
+	local f = io.open(name, "r")
+	return f ~= nil and io.close(f)
+end
+
+local global_key = home .. '/linear-api-key.txt.gpg'
+local project_key = vim.fn.getcwd() .. '/linear-api-key.txt.gpg'
+
 require('linear').setup({
-	api_key_cmd = 'gpg -d ' .. home .. '/linear-api-key.txt.gpg',
+	api_key_cmd = 'gpg -d ' .. (file_exists(project_key) and project_key or global_key),
 })
