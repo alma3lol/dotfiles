a0a28fea3e3f902177642a1ba45e22ebccac618a
Omar Almalol <omar.alma3lol@gmail.com>
2024-01-25 18:19:21 +0200

    refactor(eww): refactors top bar to be always open but not always visible

--- .config/eww/assets/scripts/toggle-bar.sh
+++ .config/eww/assets/scripts/toggle-bar.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-monitors_count=`bspc query -M | wc -l`
 current_top_padding=$(bspc config top_padding)
 if [ $current_top_padding = "60" ]; then
 	bspc config top_padding 5
-	eww close bar0
-	if [[ $monitors_count -gt 1 ]]; then eww close bar1; fi
-	if [[ $monitors_count -gt 2 ]]; then eww close bar2; fi
+	eww update bar_visible=false
 else
 	bspc config top_padding 60
-	eww open bar0
-	if [[ $monitors_count -gt 1 ]]; then eww open bar1; fi
-	if [[ $monitors_count -gt 2 ]]; then eww open bar2; fi
+	eww update bar_visible=true
 fi
