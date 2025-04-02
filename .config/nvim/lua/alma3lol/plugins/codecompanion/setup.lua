9cea80f892161fdc2918e230fda815cc86932f3c
Omar Almalol <omar.alma3lol@gmail.com>
2025-04-02 02:36:42 +0200

    refactor(neovim): changes default adapter for chat strategy to deep_seek

--- .config/nvim/lua/alma3lol/plugins/codecompanion/setup.lua
+++ .config/nvim/lua/alma3lol/plugins/codecompanion/setup.lua
@@ -1,16 +1,53 @@
+vim.g.codecompanion_auto_tool_mode = true
 require("codecompanion").setup({
 	strategies = {
 		chat = {
-			adapter = "qwen",
+			adapter = "deep_seek",
+			tools = {
+				["mcp"] = {
+					-- calling it in a function would prevent mcphub from being loaded before it's needed
+					callback = function() return require("mcphub.extensions.codecompanion") end,
+					description = "Call tools and resources from the MCP Servers",
+					opts = {
+						requires_approval = true,
+					}
+				}
+			}
 		},
 		inline = {
-			adapter = "qwen",
+			adapter = "coder",
+			tools = {
+				["mcp"] = {
+					-- calling it in a function would prevent mcphub from being loaded before it's needed
+					callback = function() return require("mcphub.extensions.codecompanion") end,
+					description = "Call tools and resources from the MCP Servers",
+					opts = {
+						requires_approval = true,
+					}
+				}
+			}
 		},
 	},
 	adapters = {
-		qwen = function()
+		deep_seek = function()
 			return require("codecompanion.adapters").extend("ollama", {
-				name = "qwen",
+				name = "deep_seek",
+				schema = {
+					model = {
+						default = "deepseek-r1:8b",
+					},
+					num_ctx = {
+						default = 16384,
+					},
+					num_predict = {
+						default = -1,
+					},
+				},
+			})
+		end,
+		coder = function()
+			return require("codecompanion.adapters").extend("ollama", {
+				name = "coder",
 				schema = {
 					model = {
 						default = "qwen2.5-coder",
