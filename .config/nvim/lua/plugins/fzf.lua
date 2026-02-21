fceb66397cf6b6b77efb80fec26875b98b9de72a
Omar Almalol <omar.alma3lol@gmail.com>
2026-02-22 00:16:02 +0200

    feat(neovim): configures fzf-lua for a better layout

---
+++ .config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,12 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    winopts = {
+      fullscreen = true,
+      preview = {
+        layout = "vertical",
+        vertical = "up:70%",
+      },
+    },
+  },
+}
