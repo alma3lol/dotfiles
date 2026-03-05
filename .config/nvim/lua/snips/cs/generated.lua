2e081664bb904663f150f031ed34b5bfc7282f69
Omar Almalol <omar.alma3lol@gmail.com>
2026-03-05 09:53:20 +0200

    feat(neovim): adds new snippets to c#

--- .config/nvim/lua/snips/cs/generated.lua
+++ .config/nvim/lua/snips/cs/generated.lua
@@ -2,6 +2,9 @@ local ls = require("luasnip")
 local fmt = require "luasnip.extras.fmt".fmt
 local s = ls.snippet
 local i = ls.insert_node
+local c = ls.choice_node
+local t = ls.text_node
+local sn = ls.snippet_node
 
 return {
   s(
@@ -29,6 +32,76 @@ namespace {}.Controllers
       i(4, "int"),
       i(3, "GetRandomInt"),
       i(5, "Random.Shared.Next(0, 100)"),
+    })
+  ),
+
+  s(
+    "rep",
+    fmt([=[
+[Required]
+public required {} {} {{get; set;}}
+]=], {
+      i(2, "string"),
+      i(1, "Password"),
+    })
+  ),
+
+  s(
+    "entity",
+    fmt([=[
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DarAlmazadAPI.Data.Entities;
+
+[Table("{}")]
+public class {} : BaseEntity
+{{
+  {}
+}}
+]=], {
+      i(2, "Users"),
+      i(1, "User"),
+      i(0)
+    })
+  ),
+
+  s(
+    "cm",
+    fmt([=[
+    [Route("{route}")]
+    [Http{methodType}]{authorization}
+    public async Task<IActionResult> {methodName}({args})
+    {{
+      {}
+    }}
+]=], {
+      route = i(1, "/"),
+      methodType = i(2, "Get"),
+      authorization = c(3, {
+        t(""),
+        t({ "", "[AllowAnonymous]" }),
+        t({ "", "[Authorize]" }),
+        { t({ "", "[Authorize(AuthenticationSchemes = " }), i(1, ""), t(")]") },
+        { t({ "", "[Authorize(Policy = " }),               i(1, ""), t(")]") },
+        { t({ "", "[Authorize(Roles = " }),                i(1, ""), t(")]") },
+        { t({ "", "[Authorize(AuthenticationSchemes = " }), i(1, ""), t(", Policy = "), i(2, ""), t(")]") },
+        { t({ "", "[Authorize(AuthenticationSchemes = " }), i(1, ""), t(", Roles = "), i(2, ""), t(")]") },
+        { t({ "", "[Authorize(AuthenticationSchemes = " }), i(1, ""), t(", Policy = "), i(2, ""), t(", Roles = "), i(3, ""), t(")]") },
+        { t({ "", "[Authorize(Policy = " }),               i(1, ""), t(", Roles = "),  i(2, ""), t(")]") },
+      }),
+      methodName = i(4, "MethodName"),
+      args = c(5, {
+        t(""),
+        sn(1, {
+          t("[FromBody] "),
+          i(1, "RequestType "),
+          i(2, "request"),
+        })
+      }),
+      i(0),
     })
   ),
 
