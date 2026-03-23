dd321c061ff4b1c8a6502f37cfcb800f070e6568
Omar Almalol <omar.alma3lol@gmail.com>
2026-03-23 08:58:12 +0200

    refactor(neovim): removes old lua snippets and include them into the new file

--- .config/nvim/lua/snips/lua/generated.lua
+++ .config/nvim/lua/snips/lua/generated.lua
@@ -2,16 +2,46 @@ local ls = require("luasnip")
 local fmt = require "luasnip.extras.fmt".fmt
 local s = ls.snippet
 local i = ls.insert_node
+local c = ls.choice_node
+local t = ls.text_node
 
 return {
-	s(
-		"pins",
-		fmt([=[
+  s(
+    "pins",
+    fmt([=[
 print(vim.inspect({}))
 ]=], {
-			i(1),
-		})
-	),
+      i(1),
+    })
+  ),
+  s('prettierrc',
+    fmt([[
+{{
+  "trailingComma": "es5",
+  "tabWidth": {tabWidth},
+  "semi": {semi},
+  "singleQuote": {signleQuote}
+}}
+        ]], {
+      tabWidth = c(1, { t '2', t '4', i(1) }),
+      semi = c(2, { t 'true', t 'false' }),
+      signleQuote = c(3, { t 'true', t 'false' }),
+    })
+  ),
+  s(
+    "newsnippetplaceholder",
+    fmt([=[
+local ls = require("luasnip")
+local fmt = require "luasnip.extras.fmt".fmt
+local s = ls.snippet
+local i = ls.insert_node
 
-	------------------------------------------------------ Snippets goes here
+return {{
+    ------------------------------------------------------ Snippets goes here
+}}
+]=], {
+    })
+  ),
+
+  ------------------------------------------------------ Snippets goes here
 }
