7337ea2733873aa1b3162f30603761d23cab2ac8
Omar Almalol <omar.alma3lol@gmail.com>
2026-04-23 12:13:07 +0200

    refactor(neovim): comments out <c-a> <c-x>

--- .config/nvim/lua/plugins/opencode.lua
+++ .config/nvim/lua/plugins/opencode.lua
@@ -35,12 +35,12 @@ return {
     vim.o.autoread = true -- Required for `opts.events.reload`
 
     -- Recommended/example keymaps
-    vim.keymap.set({ "n", "x" }, "<C-a>", function()
-      require("opencode").ask("@this: ", { submit = true })
-    end, { desc = "Ask opencode…" })
-    vim.keymap.set({ "n", "x" }, "<C-x>", function()
-      require("opencode").select()
-    end, { desc = "Execute opencode action…" })
+    -- vim.keymap.set({ "n", "x" }, "<C-a>", function()
+    --   require("opencode").ask("@this: ", { submit = true })
+    -- end, { desc = "Ask opencode…" })
+    -- vim.keymap.set({ "n", "x" }, "<C-x>", function()
+    --   require("opencode").select()
+    -- end, { desc = "Execute opencode action…" })
     vim.keymap.set({ "n", "t" }, "<C-.>", function()
       require("opencode").toggle()
     end, { desc = "Toggle opencode" })
